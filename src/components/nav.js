5e5015cd4d6efb8824629ce7b5438568fd8531b0
bodilov-sergey <127027107+bodilov-sergey@users.noreply.github.com>
2023-04-02 01:50:55 +0300

    Delete src directory

--- src/components/nav.js
+++
@@ -1,9 +0,0 @@
-const nav = () => {
-	const navFixed = document.querySelectorAll('.nav__fixed');
-	window.addEventListener('scroll', navOneScroll);
-	function navOneScroll() {
-		navFixed.forEach(element => scrollY > 175 ? element.classList.add('nav__active') : element.classList.remove('nav__active'));
-	}
-}
-
-export default nav
